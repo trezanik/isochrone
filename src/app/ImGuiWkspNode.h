b92b85ea228054a4e2260f43b78fd7f048584481
Trezanik Dev <dev@trezanik.org>
2025-11-24 22:15:11 +0000

    Creation of placeholder file that will be integrated in future; do not use

---
+++ src/app/ImGuiWkspNode.h
@@ -0,0 +1,52 @@
+#pragma once
+
+/**
+ * @file        src/app/ImGuiWkspNode.h
+ * @brief       Tab for a node, selected within the workspace
+ * @license     zlib (view the LICENSE file for details)
+ * @copyright   Trezanik Developers, 2014-2025
+ */
+
+
+#include "app/definitions.h"
+
+#if TZK_USING_IMGUI
+
+#include "imgui/dear_imgui/imgui.h"
+
+
+namespace trezanik {
+namespace app {
+
+
+/**
+ * .
+ */
+class ImGuiWkspNode
+{
+	TZK_NO_CLASS_ASSIGNMENT(ImGuiWkspNode);
+	TZK_NO_CLASS_COPY(ImGuiWkspNode);
+	TZK_NO_CLASS_MOVEASSIGNMENT(ImGuiWkspNode);
+	TZK_NO_CLASS_MOVECOPY(ImGuiWkspNode);
+
+private:
+protected:
+public:
+	/**
+	 * Standard constructor
+	 */
+	ImGuiWkspNode();
+
+
+	/**
+	 * Standard destructor
+	 */
+	~ImGuiWkspNode();
+};
+
+
+
+} // namespace app
+} // namespace trezanik
+
+#endif  // TZK_USING_IMGUI
