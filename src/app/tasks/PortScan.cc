7db7dd2d7fee7fdd076e70f2741308a6056c80ed
Trezanik Dev <dev@trezanik.org>
2026-04-05 01:34:42 +0100

    Setup PortScan task for actual usage
    No parsing/loading yet, but can execute - brief test only.
    
    Has hardcoded nmap path, and assumes Windows. Will add path in config in
    future update

--- src/app/tasks/PortScan.cc
+++ src/app/tasks/PortScan.cc
@@ -4,26 +4,92 @@
  * @copyright   Trezanik Developers, 2014-2026
  */
 
-#if 0
+
 #include "app/definitions.h"
 
 #include "app/tasks/PortScan.h"
+#include "app/TConverter.h"
+
+#include "engine/Context.h"
 
 #include "core/services/log/Log.h"
 #include "core/services/ServiceLocator.h"
+#include "core/util/filesystem/file.h"
+#include "core/util/hash/compile_time_hash.h"
 #include "core/error.h"
 
+#if TZK_USING_PUGIXML
+#	include <pugixml.hpp>
+#endif
+
 #include <algorithm>
+#include <sstream>
 
 
 namespace trezanik {
 namespace app {
 
 
-PortScan::PortScan(
-	const std::string& nmap_args
+const char  nodestr_docroot[] = "nmaprun";
+constexpr char  nodestr_scaninfo[] = "scaninfo";
+constexpr char  nodestr_verbose[] = "verbose";
+constexpr char  nodestr_debugging[] = "debugging";
+
+constexpr char  nodestr_host[] = "host";
+constexpr char  nodestr_status[] = "status";
+constexpr char  nodestr_address[] = "address";
+constexpr char  nodestr_hostnames[] = "hostnames";
+constexpr char  nodestr_hostname[] = "hostname";
+constexpr char  nodestr_ports[] = "ports";
+constexpr char  nodestr_extraports[] = "extraports";
+constexpr char  nodestr_extrareasons[] = "extrareasons";
+constexpr char  nodestr_port[] = "port";
+constexpr char  nodestr_state[] = "state";
+constexpr char  nodestr_service[] = "service";
+constexpr char  nodestr_cpe[] = "cpe";
+constexpr char  nodestr_times[] = "times";
+
+constexpr char  nodestr_runstats[] = "runstats";
+constexpr char  nodestr_finished[] = "finished";
+constexpr char  nodestr_hosts[] = "hosts";
+
+const char  attrstr_addr[] = "addr";
+const char  attrstr_addrtype[] = "addrtype"; 
+const char  attrstr_args[] = "args";
+const char  attrstr_confidence[] = "conf";
+const char  attrstr_count[] = "count";
+const char  attrstr_down[] = "down";
+const char  attrstr_end[] = "endtime";
+const char  attrstr_exinfo[] = "extrainfo";
+const char  attrstr_level[] = "level";
+const char  attrstr_method[] = "method";
+const char  attrstr_name[] = "name";
+const char  attrstr_nmaprun_start[] = "start";
+const char  attrstr_num_services[] = "numservices";
+const char  attrstr_portid[] = "portid";
+const char  attrstr_product[] = "product";
+const char  attrstr_proto[] = "protocol";
+const char  attrstr_reason[] = "reason";
+const char  attrstr_reason_ttl[] = "reason_ttl";
+const char  attrstr_scanner[] = "scanner";
+const char  attrstr_services[] = "services";
+const char  attrstr_start[] = "starttime";
+const char  attrstr_startstr[] = "startstr";
+const char  attrstr_state[] = "state";
+const char  attrstr_total[] = "total";
+const char  attrstr_type[] = "type";
+const char  attrstr_up[] = "up";
+const char  attrstr_version[] = "version";
+const char  attrstr_xmloutputversion[] = "xmloutputversion";
+
+const bool  redirect_output = false;
+
+
+PortScanTask::PortScanTask(
+	port_scan_task_params params
 )
-: Task(std::bind(&PortScan::Invoke, this))
+: Task(std::bind(&PortScanTask::Invoke, this))
+, my_params(params)
 {
 	using namespace trezanik::core;
 
@@ -35,7 +101,7 @@ PortScan::PortScan(
 }
 
 
-PortScan::~PortScan()
+PortScanTask::~PortScanTask()
 {
 	using namespace trezanik::core;
 
@@ -47,27 +113,509 @@ PortScan::~PortScan()
 }
 
 
-int
-PortScan::Invoke()
+std::string
+PortScanTask::GenerateCommandArgs() const
 {
 	using namespace trezanik::core;
 
-	core::aux::ip_address  ipaddr = GetTarget();
-	TZK_LOG_FORMAT(LogLevel::Debug, "Initiating nmap port scan against %s", aux::ipaddr_to_string(ipaddr).c_str());
+	std::stringstream  ss;
+	std::string  target;
+	
+	auto wdat = my_params.wksp->GetWorkspaceData();
 
+	assert(my_params.node_uuid != blank_uuid);
+	// no credentials needed
+
+	/*
+	 * Special case; nmap outputs in XML, so we can use the written file as-is
+	 * without any of our custom handling.
+	 * Only flaw is needing to keep up with nmap changes to XML structure, but
+	 * this is stable by now so minimal grief.
+	 */
+	std::string  datpath = my_params.wksp->GenerateDataFileName(my_params.node_uuid, uuid_port_scan, redirect_output);
+	
+	if ( datpath.empty() )
+	{
+		TZK_LOG(LogLevel::Warning, "Output path generation returned empty string");
+		return "";
+	}
+
+	ss << "-oX " << datpath << " --no-stylesheet";
+
+	if ( my_params.aggressive )
+		ss << " -A";
+	if ( my_params.ping_only )
+		ss << " -sn";
+	if ( my_params.treat_all_hosts_online )
+		ss << " -Pn";
+	if ( my_params.udp )
+		ss << " -sU";
+
+	if ( my_params.timing != 0 )
+		ss << " -T" << (my_params.timing - 1);
+
+	bool  has_specifics = !my_params.specific_ports.empty();
+	bool  has_range = my_params.range_start_port != 0 && my_params.range_end_port != 0;
+
+	if ( has_specifics || has_range )
+	{
+		// this would be easy enough to mix port types; maybe add in future
+		ss << " -p";
+		if ( has_specifics )
+		{
+			for ( auto& p : my_params.specific_ports )
+			{
+				if ( *my_params.specific_ports.cbegin() != p )
+					ss << ",";
+				ss << p;
+			}
+		}
+		if ( has_range )
+		{
+			if ( !my_params.specific_ports.empty() )
+				ss << ",";
+			ss << my_params.range_start_port << "-" << my_params.range_end_port;
+		}
+	}
+
+	for ( auto& n : wdat.nodes )
+	{
+		if ( n->id == my_params.node_uuid )
+		{
+			/*
+			 * So this is no longer ping monitor only; rename to main_target_uuid?
+			 */
+			if ( n->pingmonitor_target_uuid != core::blank_uuid )
+			{
+				for ( auto& tgt : n->targets )
+				{
+					if ( tgt.uuid == n->pingmonitor_target_uuid )
+					{
+						target = tgt.target;
+						break;
+					}
+				}
+			}
+			break;
+		}
+	}
+	// if we can't get a target, no point executing
+	if ( target.empty() )
+	{
+		TZK_LOG_FORMAT(LogLevel::Warning, "Target for node %s not found", my_params.node_uuid.GetCanonical());
+		return "";
+	}
+
+	ss << " " << target;
+
+	return ss.str();
 }
 
 
-std::string
-PortScan::TaskDetail() const
+int
+PortScanTask::Invoke()
 {
-	std::string  retval = "nmap ";
-	retval += args;
-	retval += target;
-	return retva;
+	using namespace trezanik::core;
+	using namespace trezanik::core::aux;
+
+	int  retval = ErrIMPL;
+
+	// pull/determine file from task input. wkspid/nodeid.typeid.acqtime.dat
+	std::string  fpath = my_params.wksp->GenerateDataFileName(my_params.node_uuid, uuid_port_scan, redirect_output);
+
+	// if we can't write out the data, no point executing anything
+	if ( fpath.empty() )
+	{
+		// log
+		retval = ErrFAILED;
+		return retval;
+	}
+
+	// we could (and should) provide an explicit nmap executable path
+
+
+	try
+	{
+		CommonExec  c(fpath, this, redirect_output);
+		retval = c.Exec("C:\\Program Files (x86)\\Nmap\\nmap");
+	}
+	catch ( std::exception& e )
+	{
+		TZK_LOG_FORMAT(LogLevel::Error, "Exception: %s", e.what());
+		retval = ErrFAILED;
+	}
+	catch ( ... )
+	{
+		TZK_LOG(LogLevel::Error, "Exception");
+		retval = ErrFAILED;
+	}
+
+	return retval;
+}
+
+
+
+
+
+#if TZK_USING_PUGIXML
+
+void
+parse_extra_ports(
+	pugi::xml_node xml_extra_ports,
+	std::shared_ptr<port_scan_data> ptr
+)
+{
+	using namespace trezanik::core::aux;
+
+	const auto  extra_hash = TZK_COMPILE_TIME_HASH(nodestr_extrareasons);
+	auto  attr_state = xml_extra_ports.attribute(attrstr_state);
+	auto  attr_count = xml_extra_ports.attribute(attrstr_count);
+
+	if ( attr_state )
+	{
+		// unused
+	}
+	if ( attr_count )
+	{
+		// unused
+	}
+
+	for ( auto& xml_elem : xml_extra_ports.children() )
+	{
+		switch ( runtime_fnv1a_hash(xml_elem.name()) )
+		{
+		case extra_hash:
+			// nowhere to add at the moment
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+void
+parse_hostnames(
+	pugi::xml_node xml_hostnames,
+	std::shared_ptr<port_scan_data> ptr
+)
+{
+	using namespace trezanik::core::aux;
+
+	const auto  hostname_hash = TZK_COMPILE_TIME_HASH(nodestr_hostname); 
+
+	for ( auto& xml_elem : xml_hostnames.children() )
+	{
+		switch ( runtime_fnv1a_hash(xml_elem.name()) )
+		{
+		case hostname_hash:
+			{
+				// unused
+#if 0
+				auto  attr_name = xml_elem.attribute(attrstr_name);
+				auto  attr_type = xml_elem.attribute(attrstr_type);
+				ptr->hostnames.push_back({attr_name.value(), attr_type.value()});
+#endif
+			}
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+void
+parse_port(
+	pugi::xml_node xml_port,
+	std::shared_ptr<port_scan_data> ptr
+)
+{
+	using namespace trezanik::core::aux;
+
+	auto  attr_proto = xml_port.attribute(attrstr_proto);
+	auto  attr_portid = xml_port.attribute(attrstr_portid);
+
+	open_port_info  pinf;
+	pinf.proto = TConverter<IPProto>::FromString(attr_proto.value());
+	pinf.port = static_cast<uint16_t>(attr_portid.as_uint());
+
+	const auto  cpe_hash = TZK_COMPILE_TIME_HASH(nodestr_cpe);
+	const auto  service_hash = TZK_COMPILE_TIME_HASH(nodestr_service);
+	const auto  state_hash = TZK_COMPILE_TIME_HASH(nodestr_state);
+
+	for ( auto& xml_elem : xml_port.children() )
+	{
+		switch ( runtime_fnv1a_hash(xml_elem.name()) )
+		{
+		case service_hash:
+			{
+				auto  attr_name = xml_elem.attribute(attrstr_name);
+				auto  attr_prod = xml_elem.attribute(attrstr_product);
+				auto  attr_vers = xml_elem.attribute(attrstr_version);
+				auto  attr_info = xml_elem.attribute(attrstr_exinfo);
+				auto  attr_meth = xml_elem.attribute(attrstr_method);
+				auto  attr_conf = xml_elem.attribute(attrstr_confidence);
+				auto  cpe = xml_elem.child(nodestr_cpe);
+
+				if ( attr_name )
+				{
+					pinf.svc_name = attr_name.value();
+				}
+				if ( attr_prod )
+				{
+					pinf.svc_version = attr_prod.value();
+				}
+				if ( attr_vers )
+				{
+					pinf.svc_version += " ";
+					pinf.svc_version += attr_vers.value();
+				}
+				if ( cpe )
+				{
+					pinf.cpe = cpe.text().as_string();
+				}
+			}
+			break;
+		case state_hash:
+			{
+				auto  attr_stat = xml_elem.attribute(attrstr_state);
+				auto  attr_resn = xml_elem.attribute(attrstr_reason);
+				auto  attr_rttl = xml_elem.attribute(attrstr_reason_ttl);
+
+			}
+			break;
+		default:
+			break;
+		}
+	}
+
+	ptr->collection.push_back(pinf);
+}
+
+void
+parse_ports(
+	pugi::xml_node xml_ports,
+	std::shared_ptr<port_scan_data> ptr
+)
+{
+	using namespace trezanik::core;
+	using namespace trezanik::core::aux;
+
+	const auto  extra_hash = TZK_COMPILE_TIME_HASH(nodestr_extraports);
+	const auto  port_hash = TZK_COMPILE_TIME_HASH(nodestr_port);
+
+	for ( auto& xml_elem : xml_ports.children() )
+	{
+		switch ( runtime_fnv1a_hash(xml_ports.name()) )
+		{
+		case extra_hash: parse_extra_ports(xml_elem, ptr); break;
+		case port_hash:  parse_port(xml_elem, ptr); break;
+		default:
+			TZK_LOG_FORMAT(LogLevel::Info, "Unhandled element: %s", xml_elem.name());
+			break;
+		}
+	}
+}
+
+void
+parse_host(
+	pugi::xml_node xml_host,
+	std::shared_ptr<port_scan_data> ptr
+)
+{
+	using namespace trezanik::core::aux;
+
+	const auto  address_hash = TZK_COMPILE_TIME_HASH(nodestr_address); 
+	const auto  hostnames_hash = TZK_COMPILE_TIME_HASH(nodestr_hostnames);
+	const auto  ports_hash = TZK_COMPILE_TIME_HASH(nodestr_ports);
+	const auto  status_hash = TZK_COMPILE_TIME_HASH(nodestr_status);
+	const auto  times_hash = TZK_COMPILE_TIME_HASH(nodestr_times);
+	auto  attr_start = xml_host.attribute(attrstr_start);
+	auto  attr_end = xml_host.attribute(attrstr_end);
+
+	if ( attr_start )
+	{
+		// unused
+	}
+	if ( attr_end )
+	{
+		// unused
+	}
+
+	for ( auto& xml_elem : xml_host.children() )
+	{
+		switch ( runtime_fnv1a_hash(xml_elem.name()) )
+		{
+		case status_hash:
+			{
+				auto  attr_state = xml_elem.attribute(attrstr_state);
+				auto  attr_reason = xml_elem.attribute(attrstr_reason);
+				auto  attr_reason_ttl = xml_elem.attribute(attrstr_reason_ttl);
+			}
+			break;
+		case times_hash:
+			{
+				// unused
+				//auto  attr_srtt = xml_elem.attribute(attrstr_srtt);
+				//auto  attr_rttvar = xml_elem.attribute(attrstr_rttvar);
+				//auto  attr_to = xml_elem.attribute(attrstr_to);
+			}
+			break;
+		case address_hash:
+			{
+				auto  attr_addr = xml_elem.attribute(attrstr_addr);
+				auto  attr_addrtype = xml_elem.attribute(attrstr_addrtype);
+			}
+			break;
+		case hostnames_hash: parse_hostnames(xml_elem, ptr); break;
+		case ports_hash: parse_ports(xml_elem, ptr); break;
+		default:
+			break;
+		}
+	}
+}
+
+void
+parse_runstats(
+	pugi::xml_node xml_runstats,
+	std::shared_ptr<port_scan_data> ptr
+)
+{
+	using namespace trezanik::core::aux;
+
+	const auto  finished_hash = TZK_COMPILE_TIME_HASH(nodestr_finished);
+	const auto  hosts_hash = TZK_COMPILE_TIME_HASH(nodestr_hosts);
+
+	for ( auto& xml_elem : xml_runstats.children() )
+	{
+		switch ( runtime_fnv1a_hash(xml_elem.name()) )
+		{
+		case finished_hash:
+			{
+			}
+			break;
+		case hosts_hash:
+			{
+				auto  attr_up = xml_elem.attribute(attrstr_up);
+				auto  attr_down = xml_elem.attribute(attrstr_down);
+				auto  attr_total = xml_elem.attribute(attrstr_total);
+			}
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+#endif  // TZK_USING_PUGIXML
+
+
+
+
+int
+PortScanParser::Parse(
+	std::shared_ptr<fdata> objdata,
+	std::string& str_buf
+)
+{
+	using namespace trezanik::core;
+	using namespace trezanik::core::aux;
+
+	auto  ptr = std::dynamic_pointer_cast<port_scan_data>(objdata);
+
+#if TZK_USING_PUGIXML
+
+	bool  case_sens = true;
+
+	pugi::xml_document  doc;
+	pugi::xml_parse_result  parse_res = doc.load_string(str_buf.c_str());
+
+	if ( parse_res.status != pugi::status_ok )
+	{
+		TZK_LOG(LogLevel::Error, "[pugixml] Failed to load from supplied buffer");
+		return ErrEXTERN;
+	}
+
+	/*
+	 * This is nmaps XML output; we have had no input to the file contents!
+	 */
+
+	pugi::xml_node  xml_root = doc.child(nodestr_docroot);
+
+	if ( !xml_root )
+	{
+		auto  fc = doc.first_child();
+		TZK_LOG_FORMAT(LogLevel::Error, "[pugixml] Mismatched document root element: %s != %s", nodestr_docroot, fc ? fc.name() : "<none>");
+		return EINVAL;
+	}
+
+	auto  attr_scanner = xml_root.attribute(attrstr_scanner);
+	auto  attr_args = xml_root.attribute(attrstr_args);
+	auto  attr_start = xml_root.attribute(attrstr_nmaprun_start);
+	auto  attr_startstr = xml_root.attribute(attrstr_startstr);
+	auto  attr_version = xml_root.attribute(attrstr_version);
+	auto  attr_xml_version = xml_root.attribute(attrstr_xmloutputversion);
+	const auto  dbg_hash = TZK_COMPILE_TIME_HASH(nodestr_debugging);
+	const auto  hst_hash = TZK_COMPILE_TIME_HASH(nodestr_host); 
+	const auto  rst_hash = TZK_COMPILE_TIME_HASH(nodestr_runstats);
+	const auto  sci_hash = TZK_COMPILE_TIME_HASH(nodestr_scaninfo);
+	const auto  vbs_hash = TZK_COMPILE_TIME_HASH(nodestr_verbose);
+
+	if ( !attr_xml_version )
+	{
+		TZK_LOG_FORMAT(LogLevel::Warning, "No '%s' attribute", attrstr_xmloutputversion);
+	}
+	else
+	{
+		TZK_LOG_FORMAT(LogLevel::Info, "XML output version: %s", attr_xml_version.value());
+	}
+
+	if ( !attr_version )
+	{
+		TZK_LOG_FORMAT(LogLevel::Warning, "No '%s' attribute", attrstr_version);
+	}
+	else
+	{
+		TZK_LOG_FORMAT(LogLevel::Info, "nmap version: %s", attr_version.value());
+		//ptr->nmap_version;
+	}
+
+
+	for ( auto& xml_elem : xml_root.children() )
+	{
+		switch ( runtime_fnv1a_hash(xml_elem.name()) )
+		{
+		case sci_hash:
+			{
+				auto  attr_type = xml_elem.attribute(attrstr_type);
+				auto  attr_proto = xml_elem.attribute(attrstr_proto);
+				auto  attr_num_svcs = xml_elem.attribute(attrstr_num_services);
+				auto  attr_svcs = xml_elem.attribute(attrstr_services);
+			}
+			break;
+		case vbs_hash:
+			{
+				auto  attr_level = xml_elem.attribute(attrstr_level);
+			}
+			break;
+		case dbg_hash:
+			{
+				auto  attr_level = xml_elem.attribute(attrstr_level);
+			}
+			break;
+		case hst_hash:  parse_host(xml_elem, ptr); break;
+		case rst_hash:  parse_runstats(xml_elem, ptr); break;
+		default:
+			break;
+		}
+	}
+
+#else
+	return ErrIMPL;
+#endif
+
+	return ErrNONE;
 }
 
 
 } // namespace app
 } // namespace trezanik
-#endif
--- src/app/tasks/PortScan.h
+++ src/app/tasks/PortScan.h
@@ -11,62 +11,197 @@
 #include "app/definitions.h"
 
 #include "app/tasks/Task.h"
+#include "app/ForensicData.h"  // we're a forensic data item
+#include "app/Workspace.h"  // IPProto
+
+#include "core/UUID.h"
 
 
 namespace trezanik {
 namespace app {
 
 
-/**
- * The default arguments passed to nmap if nothing is provided.
- * In all cases, a single target host is assumed. If you need the equivalent
- * command directed to multiple systems, supply the arguments yourself.
- */
-std::string  default_nmap_args = "-A -T4 -v -Pn";
+class Workspace;
+
+constexpr char  idstr_port_scan[] = "c739ef28-efec-4a01-92f0-336b1ded9e8d";
+constexpr uint32_t  cth_port_scan = core::aux::compile_time_crc32_hash(idstr_port_scan);
+static trezanik::core::UUID  uuid_port_scan(idstr_port_scan);
 
 
 /**
- * .
+ * Static-method class for parsing PortScan output
  */
-class PortScan
+class PortScanParser
+{
+public:
+	/**
+	 * Definition of ParseForensicsData invocation
+	 * 
+	 * @copydoc ParseForensicsData
+	 */
+	static int
+	Parse(
+		std::shared_ptr<fdata> data,
+		std::string& strdata
+	);
+};
+
+
+
+
+struct open_port_info
+{
+	// proto
+	IPProto  proto;
+
+	// port number
+	uint16_t  port;
+
+	// service
+	std::string  svc_name;
+	std::string  svc_version;
+
+	// common platform enumeration (nmap-specific discovery)
+	std::string  cpe;
+
+	/** e.g. syn-ack */
+	std::string  open_reason;
+	
+};
+
+
+
+struct port_scan_data : public fdata
+{
+	std::vector<open_port_info>  collection;
+
+	// details of scan; start+end, verbosity, ports checked, methods
+
+
+	void
+	Accept(
+		fdata_visitor* visitor
+	) override
+	{
+		visitor->Visit(this);
+	}
+};
+
+
+/**
+ * Configuration for the port scan task
+ * 
+ * Represents nmap operations and configuration; limited to what we support.
+ * Always implies '-oX' (XML output)
+ * 
+ * Only supports a single node target at present; to be expanded to cover a
+ * group in future.
+ * 
+ * Not supported:
+ * - Mixing TCP and UDP (e.g. -p U:53,T:80)
+ */
+struct port_scan_task_params
+{
+	/**
+	 * Workspace owning the node, and the controller of destination data to disk
+	 * 
+	 * The workspace is made aware of this task and its need for retention by
+	 * invoking the OpenDataFile method, which includes the task ID, which is the
+	 * only way to identify from the dispatched event the task has ceased.
+	 */
+	std::shared_ptr<Workspace>  wksp;
+
+	/** ID of the credential set to use within the workspace */
+	trezanik::core::UUID  creds = core::blank_uuid;
+
+	// used for target lookup, file naming
+	trezanik::core::UUID  node_uuid = core::blank_uuid;
+
+	/** Enable OS & version detection, script scanning and traceroute */
+	bool  aggressive = false;
+
+	/** Perform ICMP ping only, no port scan */
+	bool  ping_only = false;
+
+	/** Skip pinging all targets to determine online state */
+	bool  treat_all_hosts_online = true;
+
+	/** Perform a UDP scan instead of TCP (default) */
+	bool  udp = false;
+
+	/**
+	 * Timing template between 1-6; higher is faster, and therefore noisier.
+	 * 
+	 * 0 is unconfigured; if set, maps to nmap 0-5
+	 */
+	uint8_t  timing = 0;
+
+	/**
+	 * If wanting a specific set of ports, populate here. Can be combined with
+	 * ranges
+	 */
+	std::vector<uint16_t>  specific_ports;
+
+	/** For a range, set this for the starting port. Range end must also be set */
+	uint16_t  range_start_port = 0;
+
+	/** For a range, set this for the starting port. Range start must also be set */
+	uint16_t  range_end_port = 0;
+};
+
+
+/**
+ * A port scan task using nmap.
+ * 
+ * Requires nmap to be installed and available in the PATH of the executing host
+ */
+class PortScanTask
 	: public Task
 {
 private:
+
+	/** Configuration parameters */
+	port_scan_task_params  my_params;
+
+	/** Product list as parsed from the task output */
+	//std::vector<port_scan>  my_scan_data;
+	port_scan_data  my_scan_data;
+
+protected:
+
+	/**
+	 * Reimplementation of Task::GenerateCommandArgs
+	 */
+	virtual std::string
+	GenerateCommandArgs() const override;
+
+
 	/**
 	 * Task method invoked from Execute
 	 *
 	 * @return
-	 *  ErrNONE if the Send+Receive executed successfully with a valid response, otherwise -1
+	 *  An error code on failure, otherwise ErrNONE
 	 */
 	int
 	Invoke();
-
-protected:
 
 public:
 	/**
 	 * Standard constructor
 	 * 
-	 * @param[in] args
-	 *  (Optional) The parameters to pass to nmap. If not supplied, the
-	 *	application default is used
+	 * @param[in] params
+	 *  The parameters to follow
 	 */
-	PortScan(
-		const std::string& args = default_nmap_args
+	PortScanTask(
+		port_scan_task_params params
 	);
 
 
 	/**
 	 * Standard destructor
 	 */
-	~PortScan();
-
-
-	virtual std::string
-	TaskDetail() const override;
+	~PortScanTask();
 };
-
-
 
 
 } // namespace app
